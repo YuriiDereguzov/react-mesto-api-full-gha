c57267eff2528e19eb29c231dcce0f52b4716b1e
Yurii Dereguzov <DereguzovYE@yandex.ru>
2023-05-16 23:14:30 +0500

    Добавляет пакет cors

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const routes = require('./routes');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+app.use(cors());
 
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {});

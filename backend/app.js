cca44f86d447e7fa21a9833dde1502a7a7d1638e
Yurii Dereguzov <DereguzovYE@yandex.ru>
2023-05-26 17:48:29 +0500

    fix: настроить cors

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(cors({ origin: ['http://localhost:3000/', 'https://projects.nomoredomains.monster/'] }));
+app.use(cors({ origin: ['http://', 'http://localhost:3000', 'https://projects.nomoredomains.monster'] }));
 
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {});
